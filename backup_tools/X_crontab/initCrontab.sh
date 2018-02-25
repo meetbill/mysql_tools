9790e11b04c23c675a4541865c549b1b800dc2da
meetbill <meetbill@163.com>
2018-02-25 11:52:07 +0800

    update:添加检测是否有 bc 命令(用于计算定时清理时间需要)

--- backup_tools/X_crontab/initCrontab.sh
+++ backup_tools/X_crontab/initCrontab.sh
@@ -5,6 +5,14 @@
 # mail: meetbill@163.com
 # Created Time: 2017-10-19 23:10:35
 #########################################################################
+function EnvCheck(){
+    bc_check=$(rpm -qa | grep ^bc | wc -l)
+    if [[ "w0" == "w${bc_check}" ]]
+    then
+        echo -e "\e[1;31mplease exe 'yum -y install bc'\e[0m"
+    fi
+	return 0
+}
 function InitCrontab(){
 	echo "initCrontab"
 	crontab -l -u root > mycron
@@ -20,4 +28,5 @@ function InitCrontab(){
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 cd ${CUR_DIR}
+EnvCheck
 InitCrontab
